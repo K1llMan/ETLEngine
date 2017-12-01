144a4dd47d2948932576317d9c1930ea547caf9c
K1llMan <k1llmanws@yandex.ru>
2017-12-01 17:04:50 +0300

    Компонент с динамической компиляцией переделан под Roslyn

--- ETLEngineCore.Components/CodeReaderWriter.cs
+++ ETLEngineCore.Components/CodeReaderWriter.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.CodeDom.Compiler;
+﻿using ETLEngineCore.Graph;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Emit;
+
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
-
-using ETLEngineCore.Graph;
-
-using Microsoft.CSharp;
 
 namespace ETLEngineCore.Components
 {
@@ -20,37 +22,64 @@ namespace ETLEngineCore.Components
             {
                 if (!parameters.ContainsKey("code"))
                     return;
-                /*
+
                 string code = parameters["code"];
-                CSharpCodeProvider provider = new CSharpCodeProvider();
-                CompilerResults results = provider.CompileAssemblyFromSource(
-                    new CompilerParameters(Assembly.GetExecutingAssembly().GetReferencedAssemblies().Select(a => a.Name + ".dll").ToArray()), code);
+                SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(code);
+                string assemblyName = Path.GetRandomFileName();
+                MetadataReference[] references = Assembly.GetExecutingAssembly().GetReferencedAssemblies()
+                    .Select(a => MetadataReference.CreateFromFile(Assembly.Load(a).Location))
+                    .Union(new MetadataReference[] {
+                        MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                        MetadataReference.CreateFromFile(typeof(Enumerable).Assembly.Location)
+                    })
+                    .ToArray();
 
-                if (results.Errors.HasErrors)
+                CSharpCompilation compilation = CSharpCompilation.Create(
+                    assemblyName,
+                    syntaxTrees: new[] { syntaxTree },
+                    references: references,
+                    options: new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+                using (var ms = new MemoryStream())
                 {
-                    return;
+                    EmitResult result = compilation.Emit(ms);
+
+                    if (!result.Success)
+                    {
+                        IEnumerable<Diagnostic> failures = result.Diagnostics.Where(diagnostic =>
+                            diagnostic.IsWarningAsError ||
+                            diagnostic.Severity == DiagnosticSeverity.Error);
+                        /*
+                        foreach (Diagnostic diagnostic in failures)
+                        {
+                            Console.Error.WriteLine("{0}: {1}", diagnostic.Id, diagnostic.GetMessage());
+                        }*/
+
+                        return;
+                    }
+
+                    ms.Seek(0, SeekOrigin.Begin);
+
+                    Type graphElementType = Assembly.Load(ms.ToArray()).GetTypes().First(t => typeof(GraphElement).IsAssignableFrom(t));
+
+                    // Код не содержал типа, унаследованного от элемента графа
+                    if (graphElementType == null)
+                        return;
+
+                    // Создаём исполняемый элемент графа
+                    GraphElement element = (GraphElement)Activator.CreateInstance(graphElementType);
+
+                    // Перенаправляем порты
+                    foreach (KeyValuePair<int, InputPort> port in InputPorts)
+                    {
+                        element.InputPorts[port.Key] = port.Value;
+                    }
+
+                    foreach (KeyValuePair<int, OutputPort> port in OutputPorts)
+                        element.OutputPorts[port.Key] = port.Value;
+
+                    element.Execute();
                 }
-
-                Type graphElementType = results.CompiledAssembly.GetTypes().First(t => typeof(GraphElement).IsAssignableFrom(t));
-
-                // Код не содержал типа, унаследованного от элемента графа
-                if (graphElementType == null)
-                    return;
-
-                // Создаём исполняемый элемент графа
-                GraphElement element = (GraphElement)Activator.CreateInstance(graphElementType);
-
-                // Перенаправляем порты
-                foreach (KeyValuePair<int, InputPort> port in InputPorts)
-                {
-                    element.InputPorts[port.Key] = port.Value;
-                }
-
-                foreach (KeyValuePair<int, OutputPort> port in OutputPorts)
-                    element.OutputPorts[port.Key] = port.Value;
-
-                element.Execute();
-                */
             }
             catch (Exception ex)
             {
